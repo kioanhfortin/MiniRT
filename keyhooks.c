0bfe6a17acb9d33ef3c6ea515473202289ecc6da
Felix <fboulang@student.42quebec.com>
2023-08-15 11:10:50 -0400

    adding old work

---
+++ keyhooks.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   keyhooks.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fboulang <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/15 10:53:34 by fboulang          #+#    #+#             */
+/*   Updated: 2023/08/15 10:53:35 by fboulang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+/*
+keyhooks nécessaire :
+croix rouge = exit;
+escape = exit;
+on destroy = exit;
+souris + roue de souris = déplacement dans la fenêtre (maybe);
+arrow keys = déplacement dans la fenêtre.
+*/
