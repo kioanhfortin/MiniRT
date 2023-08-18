a3cf357a780614d7a49d867c1e915ffa580eaa01
kioanhfortin <kioanhfortin@gmail.com>
2023-08-18 15:30:34 -0400

    add no multiple MAJ in file

--- includes/minirt.h
+++ includes/minirt.h
@@ -6,7 +6,7 @@
 /*   By: kfortin <kfortin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 10:53:57 by fboulang          #+#    #+#             */
-/*   Updated: 2023/08/18 12:48:06 by kfortin          ###   ########.fr       */
+/*   Updated: 2023/08/18 15:11:55 by kfortin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,11 +53,18 @@ char 	*ft_copy_ext(char *str, int i);
 int 	ft_tchek_file_name(char *str);
 int		ft_strcmp(char *s1, char *s2);
 int 	ft_check_point(int i, char *str);
+void	ft_check_legit(t_list *file);
+int    	ft_check_only_one_per_line(char *str);
+int     ft_is_element_maj(char *str, int i);
+int     ft_is_element_min(char *str, int i);
+int    ft_which_maj(char *str, int *flag_a, int *flag_l, int *flag_c);
 
 /*--  COLOR FONCTION  ---*/
 int	    get_rgba(int r, int g, int b, int a);
 
 /*--  ERROR FONCTION  ---*/
 void    ft_write_error(void);
+void    ft_write_error_2(t_list *file);
+void    ft_write_error_3(t_list *file);
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kfortin <kfortin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 11:06:17 by kfortin           #+#    #+#             */
-/*   Updated: 2023/08/18 12:54:15 by kfortin          ###   ########.fr       */
+/*   Updated: 2023/08/18 15:28:24 by kfortin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,15 +48,116 @@ void    ft_parsing(t_mss *mss, char *argv)
 		node = (struct s_list *)node->next;
 		i++;
     }
-    // ft_check_legit(file);
+    ft_check_legit(file);
     // ft_fetch_info(file);
     ft_print_list(file);
 }
 
-// ft_check_legit(t_list *file)
-// {
+void    ft_check_legit(t_list *file)
+{
+    int i;
+    int flag_a;
+    int flag_l;
+    int flag_c;
     
-// }
+
+    i = 0;
+    flag_a = 0;
+    flag_l = 0;
+    flag_c = 0;
+    while (file)
+    {
+        if (file->str)
+        {
+            if (ft_check_only_one_per_line(file->str) == 0)
+                ft_write_error_2(file);
+            if (ft_which_maj(file->str, &flag_a, &flag_l, &flag_c) == 0)
+                ft_write_error_3(file);
+        }
+        file = (struct s_list *)file->next;
+    }
+}
+
+int    ft_which_maj(char *str, int *flag_a, int *flag_l, int *flag_c)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] == 'A' && *flag_a == 0)
+        {
+            *flag_a = 1;
+            return (1);
+        }
+        if (str[i] == 'L' && *flag_l == 0)
+        {
+            *flag_l = 1;
+            return (1);
+        }
+        if (str[i] == 'C' && *flag_c == 0)
+        {
+            *flag_c = 1;
+            return (1);
+        }
+        if (str[i] == 'A' && *flag_a == 1)
+            return(0);
+        if (str[i] == 'L' && *flag_l == 1)
+            return(0);
+        if (str[i] == 'C' && *flag_c == 1)
+            return(0);
+        i++;
+    }
+    return (1);
+}
+
+void    ft_write_error_2(t_list *file)
+{
+    (void)file;
+    ft_putstr_fd("error file : no backslash between each element\n", 2);
+    //free file;
+    exit(0);
+}
+
+void    ft_write_error_3(t_list *file)
+{
+    (void)file;
+    ft_putstr_fd("error file : multiple maj\n", 2);
+    //free file;
+    exit(0);
+}
+
+int    ft_check_only_one_per_line(char *str)
+{
+    int i;
+    int flag;
+
+    i = 0;
+    flag = 0;
+    while (str[i])
+    {
+        if (ft_is_element_min(str, i) == 1 && ft_is_element_maj(str, i) == 1 && flag == 0)
+            flag = 1;
+        else if (ft_is_element_min(str, i) == 1 && ft_is_element_maj(str, i) == 1 && flag == 1)
+            return(0);
+        i++;
+    }
+    return (1);
+}
+
+int     ft_is_element_min(char *str, int i)
+{
+    if ((str[i] == 'p' && str[i + 1] == 'l') || (str[i] == 's' && str[i + 1] == 'p') || (str[i] == 'c' && str[i + 1] == 'y'))
+        return (1);
+    return (0);
+}
+
+int     ft_is_element_maj(char *str, int i)
+{
+    if (str[i] == 'A' || str[i] == 'C' || str[i] == 'L')
+        return (1);
+    return (0);
+}
 
 int main(int argc, char **argv)
 {
